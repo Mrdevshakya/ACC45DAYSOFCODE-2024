dc02ad46943563c2ae7c13161b1e24812ba0ed69
Mrdevshakya <devshakya666@gmail.com>
2024-10-27 23:56:20 +0530

    Add files via upload

---
+++ Day25 - CHEF&STRINGS.py
@@ -0,0 +1,8 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l = list(map(int, input().split()))
+    skip = 0
+    for i in range(1,n):
+        skip = skip + abs(l[i]-l[i-1]) - 1
+    print(skip)
