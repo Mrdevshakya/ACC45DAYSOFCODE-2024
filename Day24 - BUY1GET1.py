0a2ea66e755656f64823fb7f2e2de91ce730e698
Mrdevshakya <devshakya666@gmail.com>
2024-10-28 22:07:51 +0530

    Add files via upload

---
+++ Day24 - BUY1GET1.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+for T in range(int(input())):
+    
+    S = Counter(input().strip())
+    
+    money = 0
+    for v in S.values():
+        money += (v+1)//2
+        
+    print(money)
